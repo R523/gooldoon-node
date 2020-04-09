61e1819613104596bf9336965469a61452576422
Parham Alvani <parham.alvani@gmail.com>
2020-04-09 16:51:19 +0000

    feat: Add LED Resource Controller

--- src/main.c
+++ src/main.c
@@ -19,6 +19,8 @@
 #include "freertos/event_groups.h"
 #include "freertos/task.h"
 
+#include "driver/gpio.h"
+
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
@@ -35,6 +37,9 @@
 
 const static char *TAG = "esp32_demo";
 const static int PORT = 1378;
+
+#define LED_GPIO 18
+#define DHT11_GPIO 21
 
 /* Wifi Credentials */
 #define DEMO_WIFI_SSID "ssid"
@@ -59,6 +64,30 @@ static void hnd_elahe_get(coap_context_t *ctx, coap_resource_t *resource,
   }
 
   response_data = cJSON_Print(response_json);
+
+  coap_add_data_blocked_response(
+      resource, session, request, response, token, COAP_MEDIATYPE_TEXT_PLAIN, 0,
+      strlen(response_data), (const u_char *)response_data);
+}
+
+static void hnd_led_get(coap_context_t *ctx, coap_resource_t *resource,
+                          coap_session_t *session, coap_pdu_t *request,
+                          coap_binary_t *token, coap_string_t *query,
+                          coap_pdu_t *response) {
+  static int status = 0;
+
+  status = (status + 1) % 2;
+
+  char *response_data;
+  cJSON *response_json = cJSON_CreateObject();
+
+  if (cJSON_AddBoolToObject(response_json, "status", status) == NULL) {
+    return;
+  }
+
+  response_data = cJSON_Print(response_json);
+
+  gpio_set_level(LED_GPIO, status);
 
   coap_add_data_blocked_response(
       resource, session, request, response, token, COAP_MEDIATYPE_TEXT_PLAIN, 0,
@@ -94,6 +123,7 @@ static void coap_goldoon_server(void *p) {
       goto clean_up;
     }
 
+    /* register "elahe" */
     resource = coap_resource_init(coap_make_str_const("elahe"), 0);
     if (!resource) {
       ESP_LOGE(TAG, "coap_resource_init() failed");
@@ -104,6 +134,19 @@ static void coap_goldoon_server(void *p) {
     /* We possibly want to Observe the GETs */
     coap_resource_set_get_observable(resource, 1);
     coap_add_resource(ctx, resource);
+
+    /* register "led" */
+    resource = coap_resource_init(coap_make_str_const("led"), 0);
+    if (!resource) {
+      ESP_LOGE(TAG, "coap_resource_init() failed");
+      goto clean_up;
+    }
+
+    coap_register_handler(resource, COAP_REQUEST_GET, hnd_led_get);
+    /* We possibly want to Observe the GETs */
+    coap_resource_set_get_observable(resource, 1);
+    coap_add_resource(ctx, resource);
+
 
     wait_ms = COAP_RESOURCE_CHECK_TIME * 1000;
 
@@ -129,6 +172,10 @@ clean_up:
 }
 
 void app_main(void) {
+  gpio_pad_select_gpio(LED_GPIO);
+  /* Set the GPIO as a push/pull output */
+  gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
   ESP_ERROR_CHECK(nvs_flash_init());
   tcpip_adapter_init();
   ESP_ERROR_CHECK(esp_event_loop_create_default());
